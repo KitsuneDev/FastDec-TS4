60b9c728dc86694c0d47be9d40f18d501ab0f663
Gabriel Enrico Augusto Rocha <Gabriel Enrico Augusto Rocha>
2017-05-17 23:04:04 -0300

    Added Page

---
+++ docs/js/colorBG.js
@@ -0,0 +1,24 @@
+﻿function colorMe() {
+    var colorchoose = randomIntFromInterval(1, 3);
+    switch (colorchoose) {
+        case 1:
+            document.body.style.backgroundColor = "#0099ff";
+            break;
+
+        case 2:
+            document.body.style.backgroundColor = "#ff6600";
+            break;
+            //case 3:
+            //document.body.style.backgroundColor = "#ff00ff"; 
+        case 3:
+            document.body.style.backgroundColor = "#666699";
+            break;
+    }
+}
+
+
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+colorMe();
---
+++ docs/js/site.js
@@ -0,0 +1,7 @@
+﻿$(function(){
+// Write your Javascript code.
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('background', "/assets/particles.json", function () {
+    console.log('callback - particles.js config loaded');
+});
+});
